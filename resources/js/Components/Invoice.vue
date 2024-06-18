a69221c47f899ca0480eb39d836daa37d45f2333
Themba Real Lukhele <realthemba@gmail.com>
2024-06-18 02:10:33 +0200

    Invoice component added and imported to Dashboard

---
+++ resources/js/Components/Invoice.vue
@@ -0,0 +1,17 @@
+<!-- resources/js/Components/MyComponent.vue -->
+<template>
+    <div>
+      <h1>This is Real Invoicing system</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Invoice',
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
+  
